42fdc36ab9ff31f839b04bec4eeabf693efdf9c8
“cb-Vinothini” <vinothini@chargebee.com>
2019-12-12 18:38:00 +0530

    read data from mysql using hybernet

--- src/main/java/com/misfits/autopilot/controller/AutopilotController.java
+++ src/main/java/com/misfits/autopilot/controller/AutopilotController.java
@@ -1,14 +1,23 @@
 package com.misfits.autopilot.controller;
 
+import com.misfits.autopilot.models.EmployeeEntity;
+import com.misfits.autopilot.models.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public class AutopilotController {
 
+    @Autowired
+    EmployeeRepository repository;
+
     @RequestMapping("hello")
-    public String sayHello(@RequestParam(value = "name") String name){
-        return "Hello "+ name + "!";
+    public String sayHello(@RequestParam(value = "id") long id){
+        Optional<EmployeeEntity> emp = repository.findById(id);
+        return "Hello "+ emp.toString() + "!";
     }
 }
