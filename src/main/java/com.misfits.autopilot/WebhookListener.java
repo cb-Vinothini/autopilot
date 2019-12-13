1c7cf34e39dcc90ca4bfed5a32c48cac04225a37
“cb-Vinothini” <vinothini@chargebee.com>
2019-12-13 16:59:26 +0530

    lister fixed and port changed

--- src/main/java/com.misfits.autopilot/WebhookListener.java
+++ src/main/java/com.misfits.autopilot/WebhookListener.java
@@ -1,5 +1,6 @@
 package com.misfits.autopilot;
 
+import com.chargebee.org.json.JSONException;
 import com.chargebee.org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,9 +9,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class WebhookListener {
 
-    @RequestMapping(value = "/listener", method= RequestMethod.POST, consumes = "application/json")
-    public ResponseEntity listener(@RequestBody JSONObject event) {
-        System.out.println(event);
+    @RequestMapping(value = "/listener", method= RequestMethod.POST)
+    public ResponseEntity listener(@RequestBody String event) throws JSONException {
+        JSONObject ec = new JSONObject(event);
+        System.out.println(ec.toString());
         return new ResponseEntity(HttpStatus.OK);
     }
 }
